95e0252fbdb72c28f9eeb72f0e36333eea5a7602
robsonklima <robsonklima@gmail.com>
2018-06-08 14:44:18 -0300

    REF

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 
+
 mongoose.connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected to Mongo DB.'))
   .catch((err) => console.log('Could not connect to Mongo DB.'));
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}.`));
