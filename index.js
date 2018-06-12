29c306c8fbd364cec38c7f712fc31ee1f2d05500
robsonklima <robsonklima@gmail.com>
2018-06-12 16:39:16 -0300

    Refactoring: startups and cleaning index.js

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-require('express-async-errors');
-const mongoose = require('mongoose');
 const express = require('express');
-const config = require('config');
 const app = express();
 
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error');
-
-if (!config.get('jwtPrivateKey')) {
-  console.log('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1);
-}
-
-mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('Connected to Mongo DB.'))
-  .catch((err) => console.log('Could not connect to Mongo DB.'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
+require('./startup/logging')()
+require('./startup/routes')(app)
+require('./startup/db')()
+require('./startup/config')()
+require('./startup/validation')()
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}.`));
