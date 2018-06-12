b908f2da36623dcc327146cdc60115088647fcfe
robsonklima <robsonklima@gmail.com>
2018-06-11 23:19:15 -0300

    async middleware function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('express-async-errors');
 const mongoose = require('mongoose');
 const express = require('express');
 const config = require('config');
@@ -12,6 +13,7 @@ const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
+const error = require('./middleware/error');
 
 if (!config.get('jwtPrivateKey')) {
   console.log('FATAL ERROR: jwtPrivateKey is not defined.');
@@ -29,6 +31,7 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}.`));
