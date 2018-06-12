8cedcf80cd834587d321866a44df0fd4b161a555
robsonklima <robsonklima@gmail.com>
2018-06-11 21:24:27 -0300

    Config and Environment Variable JWT Key

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const config = require('config');
 const app = express();
 
 const Joi = require('joi');
@@ -12,6 +13,11 @@ const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 
+if (!config.get('jwtPrivateKey')) {
+  console.log('FATAL ERROR: jwtPrivateKey is not defined.');
+  process.exit(1);
+}
+
 mongoose.connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected to Mongo DB.'))
   .catch((err) => console.log('Could not connect to Mongo DB.'));
@@ -23,7 +29,6 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}.`));
--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const Joi = require('joi');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const config = require('config');
 const { User } = require('../models/user');
 
 
@@ -18,7 +19,7 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-  const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
 
   res.send(token);
 });
