58233891330257547abd0fcd62bb938a50d9b385
robsonklima <robsonklima@gmail.com>
2018-06-14 09:04:31 -0300

    REF

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-function auth(req, res, next) {
+module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
@@ -13,5 +13,3 @@ function auth(req, res, next) {
     res.status(400).send('Invalid token.');
   }
 }
-
-module.exports = auth;
